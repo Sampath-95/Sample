0072f5541b61098091f3910695b6e0418667f726
Sampath-95 <88713257+Sampath-95@users.noreply.github.com>
2023-11-12 08:19:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import streamlit as st
 
 st.balloons()
 
-st.success('🪔Wishing you a happy and prosperous Diwali🪔')
+st.text('🪔Wishing you a happy Diwali filled with moments of joy, laughter with your lovedones🪔')
